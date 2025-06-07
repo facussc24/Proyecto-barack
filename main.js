29ea30cb1aa7e06abae84bc0b8106a63f5b67848
Facundo <104790358+facussc24@users.noreply.github.com>
2025-06-07 13:49:39 -0300

    Simplify app for Electron

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 const { app, BrowserWindow } = require('electron');
-const path = require('path');
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -7,18 +6,13 @@ function createWindow() {
     height: 768,
     webPreferences: {
       nodeIntegration: true,
-      contextIsolation: false,
-      preload: path.join(__dirname, 'preload.js')
+      contextIsolation: false
     }
   });
-
   win.loadFile('index.html');
 }
 
 app.whenReady().then(createWindow);
-
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+  if (process.platform !== 'darwin') app.quit();
 });
