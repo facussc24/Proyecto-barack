8f6927e42256afdc54c788071dd362058ac6ab47
Facundo <104790358+facussc24@users.noreply.github.com>
2025-06-13 15:38:42 -0300

    Add local-file usage warning

---
+++ file-warning.js
@@ -0,0 +1,3 @@
+if (location.protocol === 'file:') {
+  alert('Para que las páginas funcionen correctamente abra el proyecto con un servidor local. Ejecute "python3 -m http.server" y navegue a http://localhost:8000/');
+}
