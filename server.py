182c2c9731282c427109d8e675d1ff0d988e08a9
Facundo <104790358+facussc24@users.noreply.github.com>
2025-06-23 21:50:02 -0300

    Fix server to run SocketIO

--- server.py
+++ server.py
@@ -357,4 +357,4 @@ if __name__ == "__main__":
         scheduler.add_job(backup_latest, "interval", days=1)
         scheduler.start()
 
-    app.run(host="0.0.0.0", port=5000)
+    socketio.run(app, host="0.0.0.0", port=5000)
